b9604b74477ee38f4f5cbd41b69f8e8352b1c16d
tomB <thombetteridge@gmail.com>
2025-05-18 22:30:42 +0930

    change

--- source/app.d
+++ source/app.d
@@ -79,7 +79,7 @@ void main()
 
 	Camera cam;
 	cam.aspect_ratio = 16.0 / 9.0;
-	cam.image_width = 1200;
+	cam.image_width = 1600;
 	cam.samples_per_pixel = 100;
 	cam.max_depth = 50;
 	cam.vfov = 20;
@@ -257,7 +257,7 @@ struct Hittable_list
 	}
 }
 
-enum MaterialType
+enum MaterialType : ubyte
 {
 	lambertian,
 	metal,
